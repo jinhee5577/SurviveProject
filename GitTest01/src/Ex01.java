d1520c4f27e9069baade272c801846e927324978
User <User@192.168.219.102>
2024-01-30 18:55:18 +0900

    main project

--- GitTest01/src/Ex01.java
+++ GitTest01/src/Ex01.java
@@ -4,39 +4,68 @@ public class Ex01 {
 
 	public static void main(String[] args) {
 		
-		System.out.println("팀장 첫번째 커밋");
-        System.out.println("팀원1 두번째 커밋 완 ");
-		System.out.println("\r\n"
-        		+ " ██░ ██▓█████ ██▓    ██▓    ▒█████      █     █░▒█████  ██▀███  ██▓   ▓█████▄ \r\n"
-        		+ "▓██░ ██▓█   ▀▓██▒   ▓██▒   ▒██▒  ██▒   ▓█░ █ ░█▒██▒  ██▓██ ▒ ██▓██▒   ▒██▀ ██▌\r\n"
-        		+ "▒██▀▀██▒███  ▒██░   ▒██░   ▒██░  ██▒   ▒█░ █ ░█▒██░  ██▓██ ░▄█ ▒██░   ░██   █▌\r\n"
-        		+ "░▓█ ░██▒▓█  ▄▒██░   ▒██░   ▒██   ██░   ░█░ █ ░█▒██   ██▒██▀▀█▄ ▒██░   ░▓█▄   ▌\r\n"
-        		+ "░▓█▒░██░▒████░██████░██████░ ████▓▒░   ░░██▒██▓░ ████▓▒░██▓ ▒██░██████░▒████▓ \r\n"
-        		+ " ▒ ░░▒░░░ ▒░ ░ ▒░▓  ░ ▒░▓  ░ ▒░▒░▒░    ░ ▓░▒ ▒ ░ ▒░▒░▒░░ ▒▓ ░▒▓░ ▒░▓  ░▒▒▓  ▒ \r\n"
-        		+ " ▒ ░▒░ ░░ ░  ░ ░ ▒  ░ ░ ▒  ░ ░ ▒ ▒░      ▒ ░ ░   ░ ▒ ▒░  ░▒ ░ ▒░ ░ ▒  ░░ ▒  ▒ \r\n"
-        		+ " ░  ░░ ░  ░    ░ ░    ░ ░  ░ ░ ░ ▒       ░   ░ ░ ░ ░ ▒   ░░   ░  ░ ░   ░ ░  ░ \r\n"
-        		+ " ░  ░  ░  ░  ░   ░  ░   ░  ░   ░ ░         ░       ░ ░    ░        ░  ░  ░    \r\n"
-        		+ "                                                                       ░      \r\n"
-        		+ "\r\n"
-        		+ "");
-		System.out.println("속보) 지니오 정신 놓음");
-		System.out.println("나 하기싫어 귀찮아");
-		System.out.println("임정민 mz네");
+		Scanner sc = new Scanner(System.in);
+
 		
-		
-		System.out.println("지니오가 남깁니다. 우리팀 사랑해요 우유빛갈 이녜팀 사랑해요 긋쳐?!~");
-		
-		System.out.println("정민 림. 어디막 놀러가 :(");
-		
-		Scanner sc = new Scanner(System.in); 
-		System.out.println("별갯수 찍어보거라 : ");
-		int star = sc.nextInt();        // 종원이가 함 ㅋ
-		for(int i=0; i<star; i++) { // 정민이가 함 ㅋ
-			for(int j=0; j<i; j++) {
-				System.out.print("*");
-			}System.out.println();
+		System.out.println("============ gameStory ===============");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("======================================");
+
+		while (true) {
+			System.out.println("방공호 에서 살아남기 ");
+			System.out.println("[1]회원가입 [2]게임시작 [3]전체랭킹조회 [4]게임설명 [5]게임종료");
+
+			int choice = sc.nextInt();
+			if (choice == 5) {
+				System.out.println("게임을 종료합니다.");
+				break;
+			} else if (choice == 1) {
+				System.out.println("회원가입 페이지");
+				System.out.print("아이디 입력 : ");
+				String input_id = sc.next();
+				System.out.print("비밀번호 입력 : ");
+				int intput_pw = sc.nextInt();
+				System.out.print("닉네임 입력 : ");
+				String input_userName = sc.next(); // id,pw,userName DB에저장
+
+				System.out.println("회원가입이 완료 되었습니다.");
+
+			} else if (choice == 2) {
+				System.out.println("게임 캐릭터 선택 : [1]진희 오. [2]정민 림. [3]인혜 나. [4]종원 오.");
+				System.out.println("진희 오. : 체력 = ? 포만감 = ? 배고픔 = ? 보유 아이템 = ?");
+				System.out.println("정민 림. : 체력 = ? 포만감 = ? 배고픔 = ? 보유 아이템 = ?");
+				System.out.println("인혜 나. : 체력 = ? 포만감 = ? 배고픔 = ? 보유 아이템 = ?");
+				System.out.println("종원 오. : 체력 = ? 포만감 = ? 배고픔 = ? 보유 아이템 = ?");
+				System.out.println("원하는 캐릭터를 선택하세요");
+				int choice2 = sc.nextInt();
+				while (true) {
+					if (choice2 == 1) {
+
+					} else if (choice2 == 2) {
+
+					} else if (choice2 == 3) {
+
+					} else if (choice2 == 4) {
+
+					}
+				}
+
+			} else if (choice == 3) {
+
+			} else if (choice == 4) {
+				System.out.println("==========================================");
+				System.out.println("게임 설명입니다.");
+				System.out.println("이게임은 방공호에서 살아남기입니다. 원하는 캐릭터를 골라");
+				System.out.println("다양한 특성과 아이템들을 활용하여 각종 위험에서 살아남으세요! ");
+				System.out.println("==========================================");
+			}
+
 		}
-		System.out.println("캐뤼함"+"스캐너는 내가함 ㅅㄱ ");
 	
 	
 	}
